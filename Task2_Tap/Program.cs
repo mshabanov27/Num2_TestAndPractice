b7a6af0f853852e30130d986b30d5e42251a5680
Mitya <mityashabanov2004@gmail.com>
2022-03-17 22:16:15 +0200

    Task2 1st try

---
+++ Task2_Tap/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task2_Tap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = FileWorker.readFromFile("file1.csv");
+            FileWorker.writeToFile("resultFile.csv", FileWorker.editText(text));
+        }
+    }
+}
