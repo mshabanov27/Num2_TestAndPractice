d989a8a6978e923af66400c549dcaa4c82a541dc
Mitya <mityashabanov2004@gmail.com>
2022-03-19 17:22:40 +0200

    CsvReader class and ReadTable method created

--- Task2_Tap/Class1.cs
+++ Task2_Tap/Class1.cs
@@ -7,6 +7,28 @@ using System.IO;
 
 namespace Task2_Tap
 {
+    class CsvReader
+    {
+        public string[][] ReadTable(string fileName)
+        {
+            var fileText = new StreamReader(fileName);
+            string results = fileText.ReadToEnd();
+            fileText.Close();
+
+            var lines = results.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+            var lineSplitters = new string[2] { ";", ":" };
+            var resultsTable = new string[lines.Length][];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                resultsTable[i] = lines[i].Split(lineSplitters, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            return resultsTable;
+        }
+    }
+
     static class FileWorker
     {
         public static string readFromFile(string name)
